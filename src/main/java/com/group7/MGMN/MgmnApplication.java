cd1ace477e7ee3ac0757f2346844da38941f3424
yujay0529 <warmyeol0987@naver.com>
2022-02-10 11:29:50 +0900

    index추가

--- src/main/java/com/group7/MGMN/MgmnApplication.java
+++ src/main/java/com/group7/MGMN/MgmnApplication.java
@@ -2,7 +2,10 @@ package com.group7.MGMN;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+import com.group7.MGMN.controller.HomeController;
+@ComponentScan(basePackageClasses = HomeController.class)
 @SpringBootApplication
 public class MgmnApplication {
 
