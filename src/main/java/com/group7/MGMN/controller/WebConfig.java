dd0daba7d37024c35c95825e850d3dd3dc405249
yujay0529 <warmyeol0987@naver.com>
2022-03-10 17:08:16 +0900

    경로수정

--- src/main/java/com/group7/MGMN/controller/WebConfig.java
+++ src/main/java/com/group7/MGMN/controller/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 		
 		registry.addResourceHandler("/voice/**")
-		.addResourceLocations("file:///upload");
+		.addResourceLocations("file:///upload/");
 		
 		
 	}
